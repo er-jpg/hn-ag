e453f6063a375c0ca7daf399a0166026dca49cdd
Bruno Saragosa <brunosaragosa@gmail.com>
2022-03-07 23:10:30 -0300

    test: added more test coverage for ets service

--- apps/ets_service/test/ets_service_test.exs
+++ apps/ets_service/test/ets_service_test.exs
@@ -20,11 +20,15 @@ defmodule EtsServiceTest do
     %{data_service: data_service, story: story}
   end
 
-  describe "insert_data/2" do
+  describe "insert_data/1" do
     test "inserts valid data into ETS", %{story: story} do
       assert Enum.empty?(EtsService.find_data(story.id))
       assert EtsService.insert_data(story) == :ok
       assert Enum.member?(EtsService.find_data(story.id), story)
+    end
+
+    test "returns an error on invalid data" do
+      assert EtsService.insert_data(:invalid) == {:error, :invalid_data}
     end
   end
 
@@ -36,6 +40,17 @@ defmodule EtsServiceTest do
 
     test "returns an empty list if there's no itens with the given key" do
       assert EtsService.find_data(:invalid) == []
+    end
+  end
+
+  describe "delete_data/1" do
+    setup %{story: story} do
+      EtsService.insert_data(story)
+    end
+
+    test "deletes given element from ets", %{story: story} do
+      assert EtsService.delete_data(story.id) == :ok
+      refute Enum.member?(EtsService.find_data(story.id), story)
     end
   end
 end
